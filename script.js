b0b276b9e6f64f5bff1f7b6fb81ab2d59cf2edc1
Kcity162 <kevin.torrington@gmail.com>
2024-08-12 16:30:11 +0100

    Added some styles

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 var name = 'Lilly';
-var balance = 500; //The user's starting balance.
+var balance = 550; //The user's starting balance.
 var target_budget = 1000; //The total budget the user has set for the month.
 var days_in_month = 31; //The total number of days in the current month.
 var spend_data = ['Item 1', 'Item 2']; //An empty list or dictionary to store the daily spend, categorized by the user.
 
 
 
-function fistFunction() { 
-    document.getElementById('balance').innerHTML = balance ;
-}
-
-fistFunction();
+document.getElementById('balance').innerHTML = balance;
 
