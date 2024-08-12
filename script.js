96c616c7ad3b22e78fb74963cdd2b5a48bcca48a
Kcity162 <kevin.torrington@gmail.com>
2024-08-12 15:01:54 +0100

    Set up vars

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = 'Lilly';
+var balance = 500; //The user's starting balance.
+var target_budget = 1000; //The total budget the user has set for the month.
+var days_in_month = 31; //The total number of days in the current month.
+var spend_data = ['Item 1', 'Item 2']; //An empty list or dictionary to store the daily spend, categorized by the user.
+
+
+
+function fistFunction() { 
+    document.getElementById('test').innerHTML = category ;
+}
+
+fistFunction();
+
